4e0d0d9ea2e724a0f728efd440ae7a9dcf8a8ec0
Niklas Karl <niklas.karl@outlook.com>
2017-04-25 01:15:07 +0200

    Update package references.

--- test/BinaryMesh.TimeSeries.Tests/TestTimeSeriesSet.cs
+++ test/BinaryMesh.TimeSeries.Tests/TestTimeSeriesSet.cs
@@ -16,12 +16,6 @@ namespace BinaryMesh.TimeSeries.Tests
         [TestMethod]
         public void TestIntegration()
         {
-            using (Stream stream = new FileStream("C:\\Users\\nikla\\Downloads\\2016-05-13_213_690_FREE_RUHSTU_02_auto4_F07.dat", FileMode.Open, FileAccess.Read))
-            {
-                ITimeSeries timeSeries = MdfTimeSeriesBuilder.BuildMdfTimeSeriesSetFromStream(stream);
-
-                timeSeries.Frames[1].GetReader();
-            }
         }
     }
 }
